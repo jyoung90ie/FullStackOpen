6b33baadacb2ddb06126aaa0ae977a994a902cc4
jyouung90ie <jyoung90ie@gmail.com>
2020-05-23 20:24:07 +0100

    Phonebook step 5
    Refactors code to improve readability.
    
    Adds two new components Filter and PersonForm, simplifying App return.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,41 @@
 import React, { useState } from 'react'
 
+const Filter = ({ searchTerm, handleSearchChange }) => {
+    return (
+        <div>
+            filter shown with <input value={searchTerm}
+                onChange={handleSearchChange} />
+        </div>
+    )
+}
+
+const PersonForm = ({ addPerson, newName, newNumber, handleNameChange, handleNumberChange }) => {
+    return (
+        <form onSubmit={addPerson}>
+            <div>
+                name: <input value={newName} onChange={handleNameChange} />
+            </div>
+            <div>
+                number: <input value={newNumber} onChange={handleNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+const Persons = ({ data }) => {
+    return data.map(person =>
+        <div key={person.name}>{person.name} {person.number}</div>)
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            number: '123-456-7890'
-        }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
@@ -64,26 +94,12 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>
-                filter shown with <input value={searchTerm} onChange={handleSearchChange} />
-            </div>
+            <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
             <h2>add a new</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange} />
-                </div>
-                <div>
-                    number: <input value={newNumber} onChange={handleNumberChange} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+            <PersonForm addPerson={addPerson} newName={newName}
+                newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
             <h2>Numbers</h2>
-
-            {personsToShow.map(person =>
-                <div key={person.name}>{person.name} {person.number}</div>
-            )}
+            <Persons data={personsToShow} />
         </div>
     )
 }
