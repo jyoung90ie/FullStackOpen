7a34a4afc84060afc8c637269f95c4dc59699e2a
jyouung90ie <jyoung90ie@gmail.com>
2020-05-24 17:02:27 +0100

    Phonebook step 6
    Adds json-server and axios packages.
    
    Json-server is used to store phonebook data and retrieve it using axios
    asynchronous promise.
    
    App updated to remove hardcoded data and retrieve from the json-server
    instead.
    
    React useEffect is used to pull data once component has rendered the first time.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ searchTerm, handleSearchChange }) => {
     return (
@@ -31,15 +32,22 @@ const Persons = ({ data }) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchTerm, setSearchTerm] = useState('')
+
+    // get data from server
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                console.log(response)
+                setPersons(response.data)
+            })
+    }, [])
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
