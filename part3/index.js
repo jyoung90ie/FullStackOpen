1c1edc50a8892adb078fad07569f9a725916af01
jyouung90ie <jyoung90ie@gmail.com>
2020-06-02 19:12:52 +0100

    Phonebook backend step 3
    Adds detail view route to view an individual person.

--- part3/index.js
+++ part3/index.js
@@ -28,14 +28,36 @@ let persons = [
     }
 ]
 
+// homepage
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook</h1>')
 })
 
+// return full persons object
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+// display detail view for a single person within the persons object
+app.get('/api/persons/:id', (request, response) => {
+    // check that id exists, otherwise return 404
+    // get id from http request headers, convert to number
+    const id = Number(request.params.id)
+    // check that id exists within the persons object
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        // if id exists then display contents 
+        response.json(person)
+    } else {
+        // id does not exist, return 404 (not found)
+        response.status(404).end()
+    }
+
+
+})
+
+// display information relating to persons object
 app.get('/info', (request, response) => {
     // get the date and convert to a text string
     const date = new Date().toString()
