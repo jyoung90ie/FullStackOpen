55bdd2174d7f578db10f047ca84018b465057fea
jyouung90ie <jyoung90ie@gmail.com>
2020-06-05 16:38:30 +0100

    Exercise 3.12
    Adds MongoDB functionality. This will be used to store persistent data
    for the application backend.
    
    To facilitate MongoDB integration, Mongoose package is used.

---
+++ part3/mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the database password, usage:')
+    console.log('node mongo.js <password>')
+}
+
+const password = process.argv[2]
+
+// connect to mongodb
+const url = `mongodb+srv://fullstack:${password}@cluster0-4cihk.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// define db schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+// initialise mongoose model
+const Person = new mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    // if only password is provided, return list of phonebook entries
+    console.log('---Show all entries in phonebook---')
+    Person
+        .find({})
+        .then(persons => {
+            persons.forEach(person => {
+                console.log(person)
+            })
+            mongoose.connection.close()
+        })
+
+
+} else {
+    // if more than 3 arguments are provided, assume user wants to add a new entry
+
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    // create new number
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+        console.log('---New entry added to the Phonebook DB---')
+        mongoose.connection.close()
+    })
+}
