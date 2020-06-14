367e4aca1f4df65bb5d5b749335e14fabbe7023e
jyouung90ie <jyoung90ie@gmail.com>
2020-06-14 15:40:49 +0100

    4.5 helper functions and unit tests, step 4
    Adds a new helper function to determine which blog post has the most
    likes. Unit tests have been added to ensure the functionality is
    working as expected.

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -65,3 +65,72 @@ describe('total likes', () => {
         expect(result).toBe(21)
     })
 })
+
+describe('favorite blog', () => {
+    test('of empty list is zero', () => {
+        const result = listHelper.favoriteBlog([])
+
+        expect(result).toEqual([])
+    })
+
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 9,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog that blog will have most likes', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+
+        expect(result).toEqual(listWithOneBlog)
+    })
+
+    const listWithManyBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Another Wonderful Post',
+            author: 'Stan',
+            url: 'http://www.dummy.url/SomeWhere/1/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a420aa71b64a676234d17f3',
+            title: 'Did I really just do that?',
+            author: 'That Met',
+            url: 'http://www.dummy.url/SomeWhere/2/',
+            likes: 3,
+            __v: 0
+        },
+        {
+            _id: '5a412aa71b24a676234d17f5',
+            title: 'Sometimes is all the times',
+            author: 'The Man',
+            url: 'http://www.dummy.url/SomeWhere/3/',
+            likes: 11,
+            __v: 0
+        },
+    ]
+
+
+
+    test('the blog with the most likes', () => {
+        const expectedResult = [{
+            _id: '5a412aa71b24a676234d17f5',
+            title: 'Sometimes is all the times',
+            author: 'The Man',
+            url: 'http://www.dummy.url/SomeWhere/3/',
+            likes: 11,
+            __v: 0
+        }]
+
+        const result = listHelper.favoriteBlog(listWithManyBlogs)
+        expect(result).toEqual(expectedResult)
+    })
+
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -10,7 +10,24 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+    // use reducer function to return maximum likes value
+    const getMaxLikes = (maxLikes, blog) => {
+        if (blog.likes > maxLikes) {
+            return blog.likes
+        }
+        return maxLikes
+    }
+
+    const maxLikes = blogs.reduce(getMaxLikes, 0)
+
+    // filter the array for the blog with the max likes and return it
+    return blogs.filter(blog => blog.likes === maxLikes)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
