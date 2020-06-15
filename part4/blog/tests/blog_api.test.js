f1876cc6b36a20168758136f896a2f6ea1b3fbc5
jyouung90ie <jyoung90ie@gmail.com>
2020-06-15 15:18:42 +0100

    4.8 Blog list tests, step 1
    Adds integration testing via the package supertest.
    
    NODE_ENV variables setup in package and used to modify the environment
    if it is a test environment (i.e. a test MONGODB database is used and
    normal logging is not displayed)

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const { initialNotes } = require('../../../.working/wpart4/notes/tests/test_helper')
+
+const initialBlogs = [
+    {
+        title: 'First Blog Post',
+        author: 'The Man',
+        url: 'http://www.google.ie/',
+        likes: 13
+    },
+    {
+        title: 'Once upon a time...',
+        author: 'The Man',
+        url: 'http://www.google.ie/',
+        likes: 2
+    }
+]
+
+beforeEach(async () => {
+    // setup dummy data before each test
+    await Blog.deleteMany({})
+
+    // convert initialBlogs into model instances
+    const blogs = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogs.map(blog => blog.save())
+
+    // do not proceed until all promises have been fulfilled
+    await Promise.all(promiseArray)
+})
+
+
+test('check that api url is working', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+})
+
+test('api returns the expected number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
