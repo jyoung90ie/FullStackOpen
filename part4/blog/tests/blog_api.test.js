26af99fa531c94a54ebd79cc5047a765fc654d66
jyouung90ie <jyoung90ie@gmail.com>
2020-06-15 16:22:20 +0100

    Blog list tests, step 3
    Adds test to check that submitting a POST api request results in the
    blog being added to the DB.

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -51,6 +51,27 @@ test('response objects contain "id" property', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
+test('api POST request adds new blogs to the database', async () => {
+    const blog = {
+        title: 'New Blog - Unique Post',
+        author: 'The Other Man',
+        url: 'http://www.google.ie/',
+        likes: 7
+    }
+
+    await api.post('/api/blogs')
+        .send(blog)
+        .expect(201)
+
+    // test that objects returned include the new blog entry
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+
+    const contents = response.body.map(r => r.title)
+    expect(contents).toContain(blog.title)
+
+})
+
 
 afterAll(() => {
     mongoose.connection.close()
