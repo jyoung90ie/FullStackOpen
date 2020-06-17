35241b68ef17498f37f0ac002714d99ec56dcfa1
jyouung90ie <jyoung90ie@gmail.com>
2020-06-17 21:14:29 +0100

    Bloglist expansion, step 5
    Implements mongoose populate method to create relationships between the
    two models, displaying the associated data.
    
    Blog controller updated to select a user from the DB for each blog. The
    blog is also concatenated to the user's blog field.

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
     response.json(blogs)
 
 })
@@ -16,10 +19,19 @@ blogsRouter.post('/', async (request, response) => {
         return response.status(400).end()
     }
 
-    const blog = new Blog(contents)
+    const user = await User.findOne({})
 
-    const newBlog = await blog.save()
-    response.status(201).json(newBlog)
+    const blog = new Blog({
+        ...contents,
+        user: user.id
+    })
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
