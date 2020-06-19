20125134d715b1847b92c42f5df00563d0cb9f87
jyouung90ie <jyoung90ie@gmail.com>
2020-06-19 18:51:38 +0100

    Blog list frontend, step 7
    Component updated to be togglable.
    
    This now displays full list of information when user clicks view button.
    
    To achieve this the component has been updated to add it's own state
    variable and function.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} <b>{blog.author}</b>
-    </div>
-)
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false)
 
+    const showWhenVisible = { display: visible ? '' : 'none' }
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+
+    const toggleVisbility = () => {
+        setVisible(!visible)
+    }
+
+    const blogStyle = {
+        marginTop: 3,
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 2
+    }
+
+    return (
+        <div style={blogStyle}>
+            <div style={hideWhenVisible}>
+                <strong>{blog.title}</strong>
+                <button onClick={toggleVisbility}>view</button>
+            </div>
+            <div style={showWhenVisible}>
+                <strong>{blog.title}</strong>
+                <button onClick={toggleVisbility}>hide</button> <br />
+                {blog.url} <br />
+                likes {blog.likes} <button>like</button> <br />
+                {blog.author}
+            </div>
+        </div>
+    )
+}
 export default Blog
