78441b855f273c0f95ecb35dd3f5cc50c6b7f39c
jyouung90ie <jyoung90ie@gmail.com>
2020-06-21 17:00:59 +0100

    Blog list frontend, step 9
    Adds sorting to blog list. Blogs will be displayed from most likest to
     least likes.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -96,7 +96,10 @@ const Blog = ({ handleSetMessage, handleSetError }) => {
 
     return (
         <>
-            {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+            {blogs
+                // sort array by number of likes
+                .sort((a, b) => b.likes - a.likes)
+                .map(blog => <Blog key={blog.id} blog={blog} />)}
         </>
     )
 
