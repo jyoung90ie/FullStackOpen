252c777d665f41354bd37389d539d6443fca074d
jyouung90ie <jyoung90ie@gmail.com>
2020-06-24 19:01:02 +0100

    Bloglist end to end testing, step 2
    Adds additional tests to verify the login functionality works as
    intended.
    
    To facilitate this test a new user is created before each. It is then
    used to verify successful login.

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,14 @@
 describe('Blog app', function () {
     beforeEach(function () {
+        // clear database
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        // setup new user
+        cy.request('POST', 'http://localhost:3001/api/users', {
+            username: 'test_user',
+            password: 'testing123',
+            name: 'Mr Tester'
+        })
+        // open app homepage
         cy.visit('http://localhost:3000')
     })
 
@@ -8,5 +16,29 @@ describe('Blog app', function () {
         cy.contains('Log in to application')
         cy.get('#username')
         cy.get('#password')
+    })
+
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
+            cy.get('#username').type('test_user')
+            cy.get('#password').type('testing123')
+            cy.get('input[type="submit"]').click()
+
+            cy.get('.success').as('success').contains('You are now logged in')
+            cy.get('@success').should('have.css', 'color', 'rgb(24, 104, 24)')
+
+            cy.contains('Welcome back Mr Tester')
+            cy.get('button').contains('logout')
+
+        })
+
+        it('fails with wrong credentials', function () {
+            cy.get('#username').type('invalid_user')
+            cy.get('#password').type('invalid_pass')
+            cy.get('input[type="submit"]').click()
+
+            cy.get('.error').as('error').contains('Error: invalid username or password')
+            cy.get('@error').should('have.css', 'border-color', 'rgb(255, 0, 0)')
+        })
     })
 })
